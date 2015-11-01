a43e94049d7f21552e99cec0e110a849e4d89db3
rherms <rlhermstein@yahoo.com>
2015-10-31 20:07:23 -0700

    Initial commit

---
+++ popup.js
@@ -0,0 +1,22 @@
+chrome.runtime.onMessage.addListener(function(request, sender) {
+  if (request.action == "getSource") {
+    message.innerText = request.source;
+  }
+});
+
+function onWindowLoad() {
+
+  var message = document.querySelector('#message');
+
+  chrome.tabs.executeScript(null, {
+    file: "getPagesSource.js"
+  }, function() {
+    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+    if (chrome.runtime.lastError) {
+      console.log('There was an error getting the page source: \n' + chrome.runtime.lastError.message);
+    }
+  });
+
+}
+
+window.onload = onWindowLoad;
